5da6c2b01b7c15c1e58e1c70ee1817373f40bfd7
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-09-14 22:24:04 -0400

    Iteration 2: hello-world
    Fixing minor typo in comments

--- go/hello-world/hello_world.go
+++ go/hello-world/hello_world.go
@@ -1,7 +1,7 @@
 // Package greeting contains a solution to the Hello World exercise.
 package greeting
 
-// HelloWorld returns the string "Hello, World!
+// HelloWorld returns the string "Hello, World!"
 func HelloWorld() string {
 	return "Hello, World!"
 }
