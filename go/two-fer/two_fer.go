40eb2e6e329d1cd6a06454c00f08d3f5bc73fa4a
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-09-14 22:34:33 -0400

    Iteration 3: two-fer
    Changing the format printing verb

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -8,5 +8,5 @@ func ShareWith(name string) string {
 	if name == "" {
 		name = "you"
 	}
-	return fmt.Sprintf("One for %v, one for me.", name)
+	return fmt.Sprintf("One for %s, one for me.", name)
 }
