441d43434783650319cea4f6e11c2f0b7387b247
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-09-14 22:33:29 -0400

    Iteration 2: two-fer
    Removing the else branch

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,4 +1,3 @@
-// Package twofer contains my answer to life
 // Package twofer contains a solution to the Two Fer exercise.
 package twofer
 
@@ -6,11 +5,8 @@ import "fmt"
 
 // ShareWith shares something with someone else
 func ShareWith(name string) string {
-	var s string
-	if name != "" {
-		s = fmt.Sprintf("One for %v, one for me.", name)
-	} else {
-		s = "One for you, one for me."
+	if name == "" {
+		name = "you"
 	}
-	return s
+	return fmt.Sprintf("One for %v, one for me.", name)
 }
