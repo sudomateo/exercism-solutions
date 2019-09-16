e93b6141bec9742cf1fe257bc4c3fdd873f8cdfe
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-09-15 23:50:21 -0400

    Iteration 1: nucleotide-count
    Initial submission

---
+++ go/nucleotide-count/nucleotide_count.go
@@ -0,0 +1,40 @@
+// Package dna contains a solution to the DNA exercise
+package dna
+
+import (
+	"errors"
+	"strings"
+)
+
+// Histogram is a mapping from nucleotide to its count in given DNA.
+type Histogram map[rune]int
+
+// DNA is a list of nucleotides. Choose a suitable data type.
+type DNA string
+
+// Counts generates a histogram of valid nucleotides in the given DNA.
+// Returns an error if d contains an invalid nucleotide.
+func (d DNA) Counts() (Histogram, error) {
+	d = DNA(strings.ToUpper(string(d)))
+	h := Histogram{
+		'A': 0,
+		'C': 0,
+		'G': 0,
+		'T': 0,
+	}
+	for _, v := range d {
+		switch v {
+		case 'A':
+			h['A']++
+		case 'C':
+			h['C']++
+		case 'G':
+			h['G']++
+		case 'T':
+			h['T']++
+		default:
+			return h, errors.New("invalid DNA")
+		}
+	}
+	return h, nil
+}
