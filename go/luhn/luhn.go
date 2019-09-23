e7547e3506eaaed479e50c7ebe31b9dea7a1c0e2
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-09-22 20:36:02 -0400

    Iteration 4: luhn
    Removing usage of strings package

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,34 +1,32 @@
 // Package luhn contains a solution to the Luhn exercise
 package luhn
 
-import (
-	"strings"
-)
-
 // Valid determines if a given string is valid per the Luhn formula
 func Valid(s string) bool {
-	s = strings.Replace(s, " ", "", -1)
 
-	if len(s) < 2 {
-		return false
-	}
+	var sum, index int
 
-	var sum int
-
-	for i, j := len(s)-1, 0; i >= 0; i, j = i-1, j+1 {
+	for i := len(s) - 1; i >= 0; i-- {
 		chr := s[i]
+		if chr == ' ' {
+			continue
+		}
 		if chr < '0' || chr > '9' {
 			return false
 		}
-
 		digit := int(chr - '0')
-		if j%2 != 0 {
+		if index%2 != 0 {
 			digit *= 2
 			if digit > 9 {
 				digit -= 9
 			}
 		}
 		sum += digit
+		index++
+	}
+
+	if index < 2 {
+		return false
 	}
 
 	return sum%10 == 0
