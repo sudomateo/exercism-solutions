c304edfba45c56f4ab89eb4a6b15c2402ce094f1
Matthew Sanabria <matthew.sanabria@sudomateo.com>
2019-09-28 10:10:58 -0400

    Iteration 2: grains
    Lowering allocations used

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,32 +1,28 @@
 // Package grains contains a solution to the Grains exercise.
 package grains
 
-import (
-	"errors"
-	"log"
-)
+import "errors"
+
+var squareError = errors.New("square: invalid input")
 
 // Square returns the number of grains for a given square
 func Square(i int) (uint64, error) {
 	if i < 1 || i > 64 {
-		return 0, errors.New("square: invalid input")
+		return 0, squareError
 	}
-	grains := 1
+	var grains uint64 = 1
 	for i-1 != 0 {
 		grains *= 2
 		i--
 	}
-	return uint64(grains), nil
+	return grains, nil
 }
 
 // Total returns the total number of grains for 64 squares
 func Total() uint64 {
-	var total uint64
-	for i := 1; i < 65; i++ {
-		grains, err := Square(i)
-		if err != nil {
-			log.Println("total: error squaring number")
-		}
+	var total, grains uint64 = 1, 1
+	for i := 0; i < 65; i++ {
+		grains *= 2
 		total += grains
 	}
 	return total
