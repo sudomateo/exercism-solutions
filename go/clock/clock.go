a6a62eabc36a64098a758a9e12306550af463081
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-10-04 23:02:34 -0400

    Iteration 3: clock
    Fixing cases where String() can operate on incorrect minutes

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -9,34 +9,38 @@ const (
 	minutesPerDay int = 1440
 )
 
-// Clock is the time in minutes
+// Clock is the time in minutes.
 type Clock struct {
 	Minutes int
 }
 
 // New contructs a new Clock.
 func New(h, m int) Clock {
-	totalMinutes := ((h * 60) + m) % minutesPerDay
-	if totalMinutes < 0 {
-		totalMinutes = minutesPerDay + totalMinutes
-	}
 	return Clock{
-		Minutes: totalMinutes,
+		Minutes: parseMinutes((h * 60) + m),
 	}
 }
 
-// String prints Clock in a specific format
+// String prints Clock in a specific format.
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.Minutes/60, c.Minutes%60)
-
+	return fmt.Sprintf("%02d:%02d", parseMinutes(c.Minutes)/60, parseMinutes(c.Minutes)%60)
 }
 
-// Add adds time to a Clock and returns a new Clock
+// Add adds time to a Clock and returns a new Clock.
 func (c Clock) Add(m int) Clock {
 	return New(0, c.Minutes+m)
 }
 
-// Subtract subtracts time from a Clock and returns a new Clock
+// Subtract subtracts time from a Clock and returns a new Clock.
 func (c Clock) Subtract(m int) Clock {
 	return New(0, c.Minutes-m)
+}
+
+// parseMinutes ensures minutes are non-negative and fall within one day.
+func parseMinutes(m int) int {
+	m %= minutesPerDay
+	if m < 0 {
+		m += minutesPerDay
+	}
+	return m
 }
