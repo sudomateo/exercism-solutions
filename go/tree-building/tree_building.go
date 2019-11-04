c251caf25535842db21c605efe209b936063a5c6
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-11-03 21:57:18 -0500

    Iteration 1: tree-building
    Initial submission

---
+++ go/tree-building/tree_building.go
@@ -0,0 +1,51 @@
+// Package Tree Building contains a solution to the Tree Building exercism.io exercise.
+package tree
+
+import "errors"
+
+type Record struct {
+	ID     int
+	Parent int
+}
+
+type Node struct {
+	ID       int
+	Children []*Node
+}
+
+func Build(records []Record) (*Node, error) {
+	// Return if no data is passed.
+	if len(records) == 0 {
+		return nil, nil
+	}
+
+	// Return if any records are outside the range and sore the records.
+	for i, r := range records {
+		if r.ID < 0 || r.ID >= len(records) {
+			return nil, errors.New("invalid record")
+		}
+		if r.ID != i {
+			records[i], records[r.ID] = records[r.ID], records[i]
+		}
+	}
+
+	// Place each node in a slice. Append the children to the respective parent.
+	nodes := make([]Node, len(records))
+	for i, r := range records {
+		if r.ID != i {
+			return nil, errors.New("non-continuous records")
+		}
+		if !((r.ID > r.Parent) || (r.ID == 0 && r.Parent == 0)) {
+			return nil, errors.New("invalid parent")
+		}
+
+		nodes[i].ID = i
+		if i != 0 {
+			p := &nodes[r.Parent]
+			p.Children = append(p.Children, &nodes[i])
+		}
+	}
+
+	// The first node in the slice is the root node. Return it to the caller.
+	return &nodes[0], nil
+}
