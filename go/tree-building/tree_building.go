35ab8e8d7bb0d5ab7edbd88d8f185854a61a17b1
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-11-05 21:10:47 -0500

    Iteration 3: tree-building
    Shortening the code

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -17,17 +17,15 @@ type Node struct {
 
 // Build parses a slice of Record structs and outputs the Node struct.
 func Build(records []Record) (*Node, error) {
+
 	// Return if no data is passed.
 	if len(records) == 0 {
 		return nil, nil
 	}
 
-	// Return if any records are outside the range and sore the records.
+	// Sort the records.
 	for i, r := range records {
-		if r.ID < 0 || r.ID >= len(records) {
-			return nil, errors.New("invalid record")
-		}
-		if r.ID != i {
+		if (r.ID != i) && (r.ID >= 0 && r.ID < len(records)) {
 			records[i], records[r.ID] = records[r.ID], records[i]
 		}
 	}
@@ -35,13 +33,9 @@ func Build(records []Record) (*Node, error) {
 	// Place each node in a slice. Append the children to the respective parent.
 	nodes := make([]Node, len(records))
 	for i, r := range records {
-		if r.ID != i {
-			return nil, errors.New("non-continuous records")
+		if (r.ID != i) || !((r.ID > r.Parent) || (r.ID == 0 && r.Parent == 0)) {
+			return nil, errors.New("invalid record")
 		}
-		if !((r.ID > r.Parent) || (r.ID == 0 && r.Parent == 0)) {
-			return nil, errors.New("invalid parent")
-		}
-
 		nodes[i].ID = i
 		if i != 0 {
 			p := &nodes[r.Parent]
