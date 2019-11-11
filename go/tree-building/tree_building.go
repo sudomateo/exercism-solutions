571229404d41bbddb0095850b5391bd6d076c03a
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-11-10 19:07:31 -0500

    Iteration 6: tree-building
    Readability improvements.

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -20,15 +20,15 @@ type Node struct {
 
 // Build parses a slice of Record structs and outputs the Node struct.
 func Build(records []Record) (*Node, error) {
-	sort.SliceStable(records, func(i, j int) bool { return records[i].ID < records[j].ID })
+	sort.SliceStable(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
 	nodes := make(map[int]*Node, len(records))
 	for i, r := range records {
 		if (r.ID != i) || !((r.ID > r.Parent) || (r.ID == 0 && r.Parent == 0)) {
 			return nil, errors.New("invalid record")
 		}
-		nodes[i] = &Node{
-			ID: i,
-		}
+		nodes[i] = &Node{ID: i}
 		if i != 0 {
 			nodes[r.Parent].Children = append(nodes[r.Parent].Children, nodes[i])
 		}
