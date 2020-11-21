96f2dc1f62581c9c3129206563091b0c2b9cb435
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2020-11-21 13:26:12 -0500

    Iteration 3: tournament
    - Move back to `sort.Slice`.
    - Move from pointer semantics to value semantics.
    - Provide more details on error cases.
    - Use a constant format string for the table rows.

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -2,11 +2,14 @@ package tournament
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
 	"sort"
 	"strings"
+)
+
+const (
+	rowFormat = "%-30s | %2v | %2v | %2v | %2v | %2v\n"
 )
 
 // team holds the statistics for a given team.
@@ -19,22 +22,9 @@ type team struct {
 	points int
 }
 
-// byPoints implements sort.Interface for []*team based on the points
-// each team has then by the team name if points are the same.
-type byPoints []*team
-
-func (p byPoints) Len() int      { return len(p) }
-func (p byPoints) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
-func (p byPoints) Less(i, j int) bool {
-	if p[i].points == p[j].points {
-		return p[i].name < p[j].name
-	}
-	return p[i].points > p[j].points
-}
-
 // Tally reads a scorecard and writes the results.
 func Tally(r io.Reader, w io.Writer) error {
-	allTeams := make(map[string]*team)
+	allTeams := make(map[string]team)
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -44,56 +34,60 @@ func Tally(r io.Reader, w io.Writer) error {
 
 		details := strings.Split(line, ";")
 		if len(details) != 3 {
-			return errors.New("failed to parse details from line")
+			return fmt.Errorf("failed to parse match details, expected 3 entries, got %d entries", len(details))
 		}
 
-		home, away, result := details[0], details[1], details[2]
+		t1, t2, result := details[0], details[1], details[2]
 
-		if _, ok := allTeams[home]; !ok {
-			allTeams[home] = &team{name: home}
-		}
+		homeTeam := allTeams[t1]
+		awayTeam := allTeams[t2]
 
-		if _, ok := allTeams[away]; !ok {
-			allTeams[away] = &team{name: away}
-		}
-
-		allTeams[home].played++
-		allTeams[away].played++
+		homeTeam.name = t1
+		awayTeam.name = t2
+		homeTeam.played++
+		awayTeam.played++
 
 		switch result {
 		case "win":
-			allTeams[home].wins++
-			allTeams[home].points += 3
-			allTeams[away].losses++
+			homeTeam.wins++
+			homeTeam.points += 3
+			awayTeam.losses++
 		case "loss":
-			allTeams[home].losses++
-			allTeams[away].wins++
-			allTeams[away].points += 3
+			homeTeam.losses++
+			awayTeam.wins++
+			awayTeam.points += 3
 		case "draw":
-			allTeams[home].draws++
-			allTeams[home].points += 1
-			allTeams[away].draws++
-			allTeams[away].points += 1
+			homeTeam.draws++
+			homeTeam.points += 1
+			awayTeam.draws++
+			awayTeam.points += 1
 		default:
-			return errors.New("invalid result")
+			return fmt.Errorf("invalid result %s, expected one of [win, loss, draw]", result)
 		}
+		allTeams[t1] = homeTeam
+		allTeams[t2] = awayTeam
 	}
 
-	var t byPoints
+	var t []team
 	for _, team := range allTeams {
 		t = append(t, team)
 	}
-	sort.Sort(byPoints(t))
+	sort.Slice(t, func(i, j int) bool {
+		if t[i].points == t[j].points {
+			return t[i].name < t[j].name
+		}
+		return t[i].points > t[j].points
+	})
 
-	_, err := fmt.Fprintf(w, "%-30s | %2s | %2s | %2s | %2s | %2s\n", "Team", "MP", "W", "D", "L", "P")
+	_, err := fmt.Fprintf(w, rowFormat, "Team", "MP", "W", "D", "L", "P")
 	if err != nil {
-		return fmt.Errorf("failed to print header: %v", err)
+		return fmt.Errorf("failed to print results header: %v", err)
 	}
 
 	for _, team := range t {
-		_, err := fmt.Fprintf(w, "%-30s | %2d | %2d | %2d | %2d | %2d\n", team.name, team.played, team.wins, team.draws, team.losses, team.points)
+		_, err := fmt.Fprintf(w, rowFormat, team.name, team.played, team.wins, team.draws, team.losses, team.points)
 		if err != nil {
-			return fmt.Errorf("failed to print data: %v", err)
+			return fmt.Errorf("failed to print results data: %v", err)
 		}
 	}
 
