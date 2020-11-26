f6c3a7bb63b58647ad9f094c0e486ad74ddb1935
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2020-11-25 19:28:55 -0500

    Iteration 2: twelve-days
    - Use a custom type to hold the verse details.
    - Write to a `bytes.Buffer` for efficiency.

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -1,31 +1,60 @@
 // Package twelve prints the lyrics to the song "The Twelve Days of Christmas".
 package twelve
 
-import "strings"
+import (
+	"bytes"
+	"fmt"
+)
+
+// verse holds what gift was given on a specific day.
+type verse struct {
+	day  string
+	gift string
+}
 
 var (
-	verses = []string{
-		"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.",
-		"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
-		"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.",
+	intro = "On the %s day of Christmas my true love gave to me: "
+	song  = []verse{
+		{day: "first", gift: "a Partridge in a Pear Tree"},
+		{day: "second", gift: "two Turtle Doves"},
+		{day: "third", gift: "three French Hens"},
+		{day: "fourth", gift: "four Calling Birds"},
+		{day: "fifth", gift: "five Gold Rings"},
+		{day: "sixth", gift: "six Geese-a-Laying"},
+		{day: "seventh", gift: "seven Swans-a-Swimming"},
+		{day: "eighth", gift: "eight Maids-a-Milking"},
+		{day: "ninth", gift: "nine Ladies Dancing"},
+		{day: "tenth", gift: "ten Lords-a-Leaping"},
+		{day: "eleventh", gift: "eleven Pipers Piping"},
+		{day: "twelfth", gift: "twelve Drummers Drumming"},
 	}
 )
 
 // Song prints the lyrics to the entire song.
 func Song() string {
-	return strings.Join(verses, "\n")
+	var b bytes.Buffer
+	for n := 1; n <= 12; n++ {
+		b.WriteString(Verse(n))
+		if n != 12 {
+			b.WriteString("\n")
+		}
+	}
+	return b.String()
 }
 
 // Verse prints a single verse in the song.
 func Verse(n int) string {
-	return verses[n-1]
+	var b bytes.Buffer
+	b.WriteString(fmt.Sprintf(intro, song[n-1].day))
+	for i := n - 1; i >= 0; i-- {
+		if n > 1 && i == 0 {
+			b.WriteString("and ")
+		}
+		b.WriteString(song[i].gift)
+		if n > 1 && i != 0 {
+			b.WriteString(", ")
+		}
+	}
+	b.WriteString(".")
+	return b.String()
 }
