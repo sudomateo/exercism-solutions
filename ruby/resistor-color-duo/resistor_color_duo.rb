b6e35928adfab4553bbe0c65e6bdebdba6ceff21
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2020-12-02 23:55:51 -0500

    Iteration 2: resistor-color-duo

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,21 +1,10 @@
 class ResistorColorDuo
-  @@resistance = {
-    "black" => 0,
-    "brown" => 1,
-    "red" => 2,
-    "orange" => 3,
-    "yellow" => 4,
-    "green" => 5,
-    "blue" => 6,
-    "violet" => 7,
-    "grey" => 8,
-    "white" => 9,
-  }
+  COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
   def self.value(bands)
     if bands.length < 2
       return 0
     end
-    @@resistance[bands[0]] * 10 + @@resistance[bands[1]]
+    COLORS.find_index(bands[0]) * 10 + COLORS.find_index(bands[1])
   end
 end
