b46157021ad9803736fcd07da43e02a905296320
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2020-12-04 17:06:53 -0500

    Iteration 2: acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,15 +1,5 @@
 class Acronym
   def self.abbreviate(str)
-    acronym = ""
-    str.split(" ").map { |word|
-      if word.include? "-"
-        word.split("-").map { |sub_word|
-          acronym += sub_word[0].upcase
-        }
-      else
-        acronym += word[0].upcase
-      end
-    }
-    acronym
+    str.scan(/\b[a-zA-Z]/).join.upcase
   end
 end
