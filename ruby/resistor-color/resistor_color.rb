4f27516e979af475f715cbad5f17cd4d2ebc9b03
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2020-12-04 00:15:23 -0500

    Iteration 2: resistor-color

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,7 +1,7 @@
 class ResistorColor
-  COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
 
   def self.color_code(color)
-    return COLORS.find_index(color)
+    return COLORS.index(color)
   end
 end
