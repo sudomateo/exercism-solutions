b20dda1e713ae25bc0fd2e90f21ab6662b05ea76
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2020-12-19 00:18:46 -0500

    Iteration 3: bank-account
    - Switched to `sync.RWMutex`.

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -5,9 +5,9 @@ import "sync"
 
 // Account represents a bank account.
 type Account struct {
+	sync.RWMutex
 	balance int64
 	closed  bool
-	sync.Mutex
 }
 
 // Open opens a new bank account.
@@ -16,12 +16,7 @@ func Open(initialDeposit int64) *Account {
 		return nil
 	}
 
-	a := Account{
-		balance: initialDeposit,
-		closed:  false,
-	}
-
-	return &a
+	return &Account{balance: initialDeposit}
 }
 
 // Close closes a bank account, returning the remaining balance and whether
@@ -44,20 +39,20 @@ func (a *Account) Close() (payout int64, ok bool) {
 // Balance returns the balance for a bank account along with whether or not the
 // bank account is closed.
 func (a *Account) Balance() (balance int64, ok bool) {
-	a.Lock()
-	defer a.Unlock()
+	a.RLock()
+	defer a.RUnlock()
 	return a.balance, !a.closed
 }
 
 // Deposit updates a bank account balance with a deposit or withdrawal,
 // returning the new balance and whether or not the operation was successful.
 func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
+	a.Lock()
+	defer a.Unlock()
+
 	if a.closed {
 		return 0, false
 	}
-
-	a.Lock()
-	defer a.Unlock()
 
 	if a.balance+amount < 0 {
 		return a.balance, false
