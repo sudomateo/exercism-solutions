eedfaa78dfff690392b8f703704c1882c2d16575
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2021-02-04 01:27:55 -0500

    Iteration 2: space-age
    - Use a map for faster lookup.
    - Fix a bug where dividing by zero was possible.

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,34 +1,29 @@
 // Package space contains a solution to the Space Age exercise.
 package space
 
+// Planet represents the name of a planet.
 type Planet string
 
-const earthYearSeconds float64 = 31557600
+// The number of seconds planet Earth takes to perform one revolution around the sun.
+const earthYear float64 = 31557600
 
 // Age calculates your age depending on what planet you're from.
 func Age(seconds float64, planet Planet) (age float64) {
-	var yearRatio float64
-
-	switch planet {
-	case "Earth":
-		yearRatio = 1
-	case "Mercury":
-		yearRatio = 0.2408467
-	case "Venus":
-		yearRatio = 0.61519726
-	case "Mars":
-		yearRatio = 1.8808158
-	case "Jupiter":
-		yearRatio = 11.862615
-	case "Saturn":
-		yearRatio = 29.447498
-	case "Uranus":
-		yearRatio = 84.016846
-	case "Neptune":
-		yearRatio = 164.79132
+	yearRatio := map[Planet]float64{
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Earth":   1,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
 	}
 
-	age = (seconds / (yearRatio * earthYearSeconds))
+	ratio, ok := yearRatio[planet]
+	if !ok {
+		return 0
+	}
 
-	return age
+	return (seconds / (ratio * earthYear))
 }
