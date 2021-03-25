aee6ad218ab94cf7ec719d31d5f51ba07228953c
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2021-03-25 16:49:28 -0400

    Iteration 3: series
    - Use private `attr_reader`.
    - Actually call the `attr_reader` instead of the instance variable.

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,13 +1,17 @@
 class Series
 
+  private
+
   attr_reader :series
+
+  public
 
   def initialize(series)
     @series = series
   end
 
   def slices(slice)
-    raise ArgumentError if slice > @series.chars.length
-    @series.chars.each_cons(slice).map(&:join)
+    raise ArgumentError if slice > series.chars.length
+    series.chars.each_cons(slice).map(&:join)
   end
 end
