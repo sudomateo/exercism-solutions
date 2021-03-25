d808175e47f5b75c8e2100b01301e8bdd3ddeb7d
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2021-03-25 00:43:29 -0400

    Iteration 2: series
    - Use `attr_reader`.
    - Simplify `initialize`.

--- ruby/series/series.rb
+++ ruby/series/series.rb
@@ -1,10 +1,13 @@
 class Series
+
+  attr_reader :series
+
   def initialize(series)
-    @series = series.chars
+    @series = series
   end
 
   def slices(slice)
-    raise ArgumentError if slice > @series.length
-    @series.each_cons(slice).map { |chars| chars.join }
+    raise ArgumentError if slice > @series.chars.length
+    @series.chars.each_cons(slice).map(&:join)
   end
 end
