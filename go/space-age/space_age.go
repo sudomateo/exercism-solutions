36bd4558949f8c993dc1ac047b3c04f9ac7ae119
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2021-04-14 13:58:41 -0400

    Iteration 3: space-age

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -1,15 +1,11 @@
 // Package space contains a solution to the Space Age exercise.
 package space
 
-// Planet represents the name of a planet.
-type Planet string
-
 // The number of seconds planet Earth takes to perform one revolution around the sun.
-const earthYear float64 = 31557600
+const earthYearInSeconds float64 = 31557600
 
-// Age calculates your age depending on what planet you're from.
-func Age(seconds float64, planet Planet) (age float64) {
-	yearRatio := map[Planet]float64{
+var (
+	yearRatio = map[Planet]float64{
 		"Mercury": 0.2408467,
 		"Venus":   0.61519726,
 		"Earth":   1,
@@ -19,11 +15,17 @@ func Age(seconds float64, planet Planet) (age float64) {
 		"Uranus":  84.016846,
 		"Neptune": 164.79132,
 	}
+)
 
+// Planet represents the name of a planet.
+type Planet string
+
+// Age calculates your age depending on what planet you're from.
+func Age(seconds float64, planet Planet) (age float64) {
 	ratio, ok := yearRatio[planet]
 	if !ok {
 		return 0
 	}
 
-	return (seconds / (ratio * earthYear))
+	return (seconds / (ratio * earthYearInSeconds))
 }
